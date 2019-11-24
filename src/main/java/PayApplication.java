74dec722c57a0ef7cf896e44421eae8b6c811c99
danchellee <583746487@qq.com>
2019-11-25 01:13:02 +0800

    Initial commit

---
+++ src/main/java/PayApplication.java
@@ -0,0 +1,7 @@
+/**
+ * @Auther: D&C
+ * @Descirption:
+ * @Time: 2019-11-25
+ */
+public class PayApplication {
+}
---
+++ src/main/java/com/danchel/web/api/PayApi.java
@@ -0,0 +1,9 @@
+package com.danchel.web.api;
+
+/**
+ * @Auther: D&C
+ * @Descirption:
+ * @Time: 2019-11-25
+ */
+public class PayApi {
+}
